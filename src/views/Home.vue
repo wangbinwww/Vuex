66e44f45fe488fdf8977a8b35662f152a27e23e8
wangbin <wangbin>
2019-06-20 09:37:16 +0800

    20190620

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,18 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="第一个演示vux演示程序"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
   }
-}
+};
 </script>
